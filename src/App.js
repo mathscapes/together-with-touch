16063a5b05e0e83468e46921bcaa7a91156dd408
0xf17 <grv@mathscapes.xyz>
2021-11-18 18:50:54 +0530

    added tracks

--- src/App.js
+++ src/App.js
@@ -6,53 +6,309 @@ import './App.css';
 import * as Tone from 'tone';
 import React, { useState } from 'react';
 import * as dat from 'dat.gui';
+import { AutoFilter } from 'tone';
+
+const AMinorScale = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+const addOctaveNumbers = (scale, octaveNumber) => scale.map(note => {
+    const firstOctaveNoteIndex = scale.indexOf('C') !== -1 ? scale.indexOf('C') : scale.indexOf('C#')
+    const noteOctaveNumber = scale.indexOf(note) < firstOctaveNoteIndex ? octaveNumber - 1 : octaveNumber;
+    return `${note}${noteOctaveNumber}`
+});
+
+const constructMajorChord = (scale, octave, rootNote) => {
+    const scaleWithOctave = addOctaveNumbers(scale, octave);
+
+    const getNextChordNote = (note, nextNoteNumber) => {
+        const nextNoteInScaleIndex = scaleWithOctave.indexOf(note) + nextNoteNumber - 1;
+        let nextNote;
+        if (typeof (scaleWithOctave[nextNoteInScaleIndex]) !== 'undefined') {
+            nextNote = scaleWithOctave[nextNoteInScaleIndex];
+        } else {
+            nextNote = scaleWithOctave[nextNoteInScaleIndex - 7];
+            const updatedOctave = parseInt(nextNote.slice(1)) + 1;
+            nextNote = `${nextNote.slice(0, 1)}${updatedOctave}`;
+        }
+
+        return nextNote;
+    }
+
+    const thirdNote = getNextChordNote(rootNote, 3)
+    const fifthNote = getNextChordNote(rootNote, 5)
+    const chord = [rootNote, thirdNote, fifthNote]
+
+    return chord
+}
+
+const constructChords = (scale, octave) => {
+    const scaleWithOctave = addOctaveNumbers(scale, octave);
+
+    const getNextChordNote = (note, nextNoteNumber) => {
+        const nextNoteInScaleIndex = scaleWithOctave.indexOf(note) + nextNoteNumber - 1;
+        let nextNote;
+        if (typeof (scaleWithOctave[nextNoteInScaleIndex]) !== 'undefined') {
+            nextNote = scaleWithOctave[nextNoteInScaleIndex];
+        } else {
+            nextNote = scaleWithOctave[nextNoteInScaleIndex - 6];
+            const updatedOctave = parseInt(nextNote.slice(1)) + 1;
+            nextNote = `${nextNote.slice(0, 1)}${updatedOctave}`;
+        }
+
+        return nextNote;
+    }
 
 
-const gui = new dat.GUI();
+    const chordArray = scaleWithOctave.map(note => {
+        let thirdNote = getNextChordNote(note, 3)
+        let fifthNote = getNextChordNote(note, 5)
 
-var basicPanel = gui.addFolder('Basic (Experimental)');
-var basic = { volume: 0.5, bpm: 80 };
-basicPanel.add(basic, 'volume', 0, 1);
+        const chord = [note, thirdNote, fifthNote]
 
-basicPanel.add(basic, 'bpm', 40, 200)
-    .onFinishChange(() => {
-        Tone.Transport.bpm.rampTo(basic.bpm, 10);
-    });
+        return chord
+    })
 
-var signalPanel = gui.addFolder('Signal (Experimental)');
-var signal = { modulation: 0.5, harmonicity: 0.3 };
-signalPanel.add(signal, 'modulation', 0, 1);
-signalPanel.add(signal, 'harmonicity', 0, 1);
+    return chordArray;
+}
 
-const notes = ["C", "D", "E", "F", "G", "A", "B"];
-const octaves = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const sharps = ["", "b"];
+Tone.Transport.bpm.value = 150
 
-const chords = [
-    ['A4', 'Cb4', 'E4'],
-    ['A4', 'C4', 'E4'],
-    ['C4', 'E4', 'G4'],
-    ['C4', 'Eb4', 'G4'],
-    ['D4', 'Fb4', 'A4'],
-    ['D4', 'F4', 'A4'],
-    ['E4', 'Gb4', 'B4'],
-    ['E4', 'G4', 'B4'],
-    ['F4', 'A4', 'C4'],
-    ['F4', 'Ab4', 'C4'],
-    ['G4', 'B4', 'D4'],
-    ['G4', 'Bb4', 'D4']
+const IChord = constructMajorChord(AMinorScale, 4, 'A3');
+const VChord = constructMajorChord(AMinorScale, 4, 'E4');
+const VIChord = constructMajorChord(AMinorScale, 3, 'F3');
+const IVChord = constructMajorChord(AMinorScale, 3, 'D3');
+
+IChord.push('A2', 'G4')
+VChord.push('E2', 'G3')
+VIChord.push('F2', 'E4')
+IVChord.push('D2', 'C4')
+
+console.log(IChord);
+console.log(VChord);
+console.log(VIChord);
+console.log(IVChord);
+
+
+const synth = new Tone.PolySynth(Tone.Synth, {
+    volume: -5,
+    oscillator: {
+        type: "sawtooth"
+    }
+}).toDestination();
+
+const mainChords = [
+    { 'time': 0, 'note': IChord, 'duration': '2n.' },
+    { 'time': '0:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '1:0', 'note': VIChord, 'duration': '2n.' },
+    { 'time': '1:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '2:0', 'note': IVChord, 'duration': '2n.' },
+    { 'time': '2:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '3:0', 'note': VIChord, 'duration': '2n' },
+    { 'time': '3:2', 'note': VChord, 'duration': '4n' },
+    { 'time': '3:3', 'note': IVChord, 'duration': '4n' },
+    { 'time': '4:0', 'note': IChord, 'duration': '2n.' },
+    { 'time': '4:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '5:0', 'note': VIChord, 'duration': '2n.' },
+    { 'time': '5:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '6:0', 'note': IVChord, 'duration': '2n.' },
+    { 'time': '6:3', 'note': VChord, 'duration': '4n' },
+    { 'time': '7:0', 'note': VIChord, 'duration': '2n' },
+    { 'time': '7:2', 'note': VChord, 'duration': '4n' },
+    { 'time': '7:3', 'note': IVChord, 'duration': '4n' },
 ];
 
-function getRandomNote() {
-    let note = notes[Math.floor(Math.random() * notes.length)];
-    let octave = octaves[Math.floor(Math.random() * octaves.length)];
-    let sharp = sharps[Math.floor(Math.random() * sharps.length)];
-    return note + sharp + octave;
-}
+const part = new Tone.Part(function (time, note) {
+    synth.triggerAttackRelease(note.note, note.duration, time);
+}, mainChords).start(0);
 
-function getRandomChord() {
-    return chords[Math.floor(Math.random() * chords.length)];
-}
+const IChord1 = constructMajorChord(AMinorScale, 5, 'A4');
+const VChord1 = constructMajorChord(AMinorScale, 5, 'E5');
+const VIChord1 = constructMajorChord(AMinorScale, 4, 'F4');
+const IVChord1 = constructMajorChord(AMinorScale, 4, 'D4');
+
+IChord.push('A3', 'G5')
+VChord.push('E3', 'D5')
+VIChord.push('F3', 'E5')
+IVChord.push('D3', 'C5')
+
+
+const mainChordPart = new Tone.PolySynth(Tone.Synth, {
+    oscillator: {
+        count: 6,
+        spread: 80,
+        type: "fatsawtooth"
+    }
+}).toDestination();
+
+const highOctaveChords = [
+    { 'time': 0, 'note': IChord1, 'duration': '2n.' },
+    { 'time': '0:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '1:0', 'note': VIChord1, 'duration': '2n.' },
+    { 'time': '1:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '2:0', 'note': IVChord1, 'duration': '2n.' },
+    { 'time': '2:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '3:0', 'note': VIChord1, 'duration': '2n' },
+    { 'time': '3:2', 'note': VChord1, 'duration': '4n' },
+    { 'time': '3:3', 'note': IVChord1, 'duration': '4n' },
+    { 'time': '4:0', 'note': IChord1, 'duration': '2n.' },
+    { 'time': '4:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '5:0', 'note': VIChord1, 'duration': '2n.' },
+    { 'time': '5:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '6:0', 'note': IVChord1, 'duration': '2n.' },
+    { 'time': '6:3', 'note': VChord1, 'duration': '4n' },
+    { 'time': '7:0', 'note': VIChord1, 'duration': '2n' },
+    { 'time': '7:2', 'note': VChord1, 'duration': '4n' },
+    { 'time': '7:3', 'note': IVChord1, 'duration': '4n' },
+];
+
+const highSynth = new Tone.PolySynth(Tone.Synth, {
+    volume: -16,
+    count: 6,
+    spread: 80,
+    oscillator: {
+        type: "fatsawtooth"
+    }
+}).toDestination();
+
+const highOctaveChordPart = new Tone.Part(function (time, note) {
+    highSynth.triggerAttackRelease(note.note, note.duration, time, 0.5);
+}, highOctaveChords).start(0);
+
+
+const mainMelody = [
+    { 'time': 0, 'note': 'G4', 'duration': '8n' },
+    { 'time': '0:0:2', 'note': 'F4', 'duration': '8n' },
+    { 'time': '0:1', 'note': 'D4', 'duration': '8n.' },
+    { 'time': '0:2', 'note': 'D4', 'duration': '8n' },
+    { 'time': '0:2:2', 'note': 'F4', 'duration': '8n.' },
+    { 'time': '0:3', 'note': 'G4', 'duration': '8n' },
+    { 'time': '0:3:2', 'note': 'A4', 'duration': '2n' },
+    { 'time': '2:0', 'note': 'A4', 'duration': '8n' },
+    { 'time': '2:0:2', 'note': 'G4', 'duration': '8n' },
+    { 'time': '2:1', 'note': 'F4', 'duration': '8n' },
+    { 'time': '2:2', 'note': 'A4', 'duration': '8n' },
+    { 'time': '2:2:2', 'note': 'G4', 'duration': '8n' },
+    { 'time': '2:3', 'note': 'E4', 'duration': '8n' },
+    { 'time': '2:3:2', 'note': 'F4', 'duration': '2n' },
+    { 'time': '4:0', 'note': 'G4', 'duration': '8n' },
+    { 'time': '4:0:2', 'note': 'F4', 'duration': '8n' },
+    { 'time': '4:1', 'note': 'D4', 'duration': '8n' },
+    { 'time': '4:2', 'note': 'F4', 'duration': '8n' },
+    { 'time': '4:2:2', 'note': 'A4', 'duration': '8n' },
+    { 'time': '4:3', 'note': 'G4', 'duration': '8n' },
+    { 'time': '4:3:2', 'note': 'A4', 'duration': '2n' },
+    { 'time': '5:2:2', 'note': 'G4', 'duration': '8n' },
+    { 'time': '5:3', 'note': 'A4', 'duration': '8n' },
+    { 'time': '5:3:2', 'note': 'B4', 'duration': '8n' },
+    { 'time': '6:0', 'note': 'C5', 'duration': '8n' },
+    { 'time': '6:1', 'note': 'B4', 'duration': '8n' },
+    { 'time': '6:1:2', 'note': 'A4', 'duration': '8n' },
+    { 'time': '6:2', 'note': 'B4', 'duration': '8n' },
+    { 'time': '6:2:2', 'note': 'A4', 'duration': '8n' },
+    { 'time': '6:3', 'note': 'G4', 'duration': '8n' },
+    { 'time': '6:3:2', 'note': 'A4', 'duration': '1n' },
+];
+
+const synth2 = new Tone.Synth({
+    oscillator: {
+        volume: 5,
+        count: 3,
+        spread: 40,
+        type: "fatsawtooth"
+    }
+}).toDestination();
+
+const mainMelodyPart = new Tone.Part(function (time, note) {
+    synth2.triggerAttackRelease(note.note, note.duration, time);
+}, mainMelody).start(0);
+
+
+const lowPass = new Tone.Filter({
+    frequency: 8000,
+}).toDestination();
+
+const snareDrum = new Tone.NoiseSynth({
+    noise: {
+        type: 'white',
+        playbackRate: 3,
+    },
+    envelope: {
+        attack: 0.001,
+        decay: 0.20,
+        sustain: 0.15,
+        release: 0.03,
+    },
+}).connect(lowPass);
+
+const snares = [
+    { time: '0:2' },
+    { time: '1:2' },
+    { time: '2:2' },
+    { time: '3:2' },
+    { time: '4:2' },
+    { time: '5:2' },
+    { time: '6:2' },
+    { time: '7:2' },
+]
+
+const snarePart = new Tone.Part(function (time) {
+    snareDrum.triggerAttackRelease('4n', time)
+}, snares).start(0);
+
+
+const kickDrum = new Tone.MembraneSynth({
+    volume: 6
+}).toDestination();
+
+const kicks = [
+    { time: '0:0' },
+    { time: '0:3:2' },
+    { time: '1:1' },
+    { time: '2:0' },
+    { time: '2:1:2' },
+    { time: '2:3:2' },
+    { time: '3:0:2' },
+    { time: '3:1:' },
+    { time: '4:0' },
+    { time: '4:3:2' },
+    { time: '5:1' },
+    { time: '6:0' },
+    { time: '6:1:2' },
+    { time: '6:3:2' },
+    { time: '7:0:2' },
+    { time: '7:1:' },
+];
+
+const kickPart = new Tone.Part(function (time) {
+    kickDrum.triggerAttackRelease('C1', '8n', time)
+}, kicks).start(0);
+
+
+const bassline = [
+    { 'time': 0, 'note': 'A0', 'duration': '2n' },
+    { 'time': '0:3', 'note': 'F0', 'duration': '2n.' },
+    { 'time': '1:3', 'note': 'D0', 'duration': '2n.' },
+    { 'time': '2:3', 'note': 'F0', 'duration': '1:1' },
+];
+
+const bass = new Tone.Synth({
+    oscillator: {
+        type: "triangle"
+    }
+}).toDestination();
+
+const bassPart = new Tone.Part(function (time, note) {
+    bass.triggerAttackRelease(note.note, note.duration, time);
+}, bassline).start(0);
+
+const noise = new Tone.Noise("pink").start();
+
+const autoFilter = new Tone.AutoFilter({
+    volume: -10,
+    frequency: "4n",
+    baseFrequency: 40,
+    octaves: 4
+}).toDestination();
+
+noise.connect(autoFilter);
 
 /**
  * Sonic generator creates algorithmic sound using realtime data from Twitter
@@ -60,73 +316,68 @@ function getRandomChord() {
 class SonicGenerator {
 
     constructor() {
-        this.noise = new Tone.Noise({
-            type: "white",
-            volume: -10
-        });
-        this.autoFilter = new Tone.AutoFilter({
-            frequency: "4n",
-            baseFrequency: 50,
-            octaves: 2
-        }).toDestination();
-        this.noise.connect(this.autoFilter);
-
-        this.synthA = new Tone.AMSynth({
-            harmonicity: signal.harmonicity,
-            modulationIndex: signal.modulation,
-            volume: -10
-        }).toDestination();
-        this.synthB = new Tone.FMSynth({
-            harmonicity: signal.harmonicity,
-            modulationIndex: signal.modulation,
-            volume: -20
-        }).toDestination();
-
-        this.loopA = new Tone.Loop(time => {
-            this.synthA.triggerAttackRelease("C1", "8n", time);
-        }, "4n").start(0);
-
-        this.loopB = new Tone.Loop(time => {
-            this.synthB.triggerAttackRelease("C2", "8n", time);
-        }, "4n").start("8n");
-
-
-        this.synth = new Tone.PolySynth().toDestination();
 
     }
 
     start() {
         Tone.Transport.start();
-        this.autoFilter.start();
-        this.noise.start();
+        autoFilter.start();
     }
 
     stop() {
         Tone.Transport.stop();
-        this.autoFilter.stop();
-        this.noise.stop();
+        autoFilter.stop();
     }
 
-    receive() {
-        this.chordEvent = new Tone.ToneEvent(((time, chord) => {
-            this.synth.triggerAttackRelease(chord, Math.random() * 1.5, time);
-        }), getRandomChord());
+    receive(words) {
+        if (words.includes("yes")) {
+            let player = new Tone.Player({
+                url: "https://tonejs.github.io/audio/drum-samples/loops/ominous.mp3",
+                autostart: true,
+            });
+            let filter = new Tone.Filter(400, 'lowpass').toDestination();
+            let feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
 
-        this.chordEvent.start();
-        this.chordEvent.loop = Math.floor(Math.random() * 8);
-        this.chordEvent.loopEnd = "1m";
+            // connect the player to the feedback delay and filter in parallel
+            player.connect(filter);
+            player.connect(feedbackDelay);
+        }
+        if (words.includes("no")) {
+            let player = new Tone.Player({
+                url: "https://tonejs.github.io/audio/berklee/gurgling_theremin_1.mp3",
+                loop: true,
+                autostart: true,
+            })
+            //create a distortion effect
+            let distortion = new Tone.Distortion(0.4).toDestination();
+            //connect a player to the distortion
+            player.connect(distortion);
+        }
     }
 }
+
+
+let SG = new SonicGenerator();
+const controls = new dat.GUI();
+
+var volume = { volume: 0.5 };
+controls.add(volume, 'volume', 0, 5);
+
+var filter = { frequency: 8000 };
+controls.add(filter, 'frequency', 0, 8000);
+
+var modulation = { modulation: 1 };
+controls.add(modulation, 'modulation', 0, 10);
 
 function App() {
     const [playing, setPlaying] = useState(false);
     const [lastMessage, setLastMessage] = useState('Hit enter to send');
     const [log, setLog] = useState([]);
 
-    let SG = new SonicGenerator();
-
     let pushToConsole = (message) => {
-        setLog(log.concat(message));
+        var d = new Date();
+        var n = d.toLocaleTimeString();
+        setLog(log.concat(n + " " + message));
         console.log(log);
     }
 
@@ -147,11 +398,10 @@ function App() {
     let testInputonKeyDown = async (event) => {
         await Tone.start();
         if (event.key === 'Enter') {
-            // setLastMessage('Received ' + event.target.value);
             pushToConsole('Received ' + event.target.value);
             let words = event.target.value.split(' ');
 
-            SG.receive();
+            SG.receive(words);
             event.target.value = '';
         }
     }
